58c551562c5434315a53f12e6797ab30fe641fe1
Samuel Drouin <samuel.drouin@sdr-soft.com>
2018-06-19 22:18:52 -0400

    Updated main directory integrity check

--- labnote/utils/integrity.py
+++ labnote/utils/integrity.py
@@ -1,45 +1,144 @@
+""" This module contains the file system integrity check for the LabNote file system """
+
 # Python import
 import os
-
-# PyQt import
-from PyQt5.QtWidgets import QMessageBox
+import sqlite3
+import logging
+from io import StringIO
 
 # Project import
-from labnote.utils import database, directory
+from labnote.utils import database, directory, fsentry
 
-NO_EXCEPTION = -1
-MAIN_DIRECTORY_CREATION_EXCEPTION = 0
-MAIN_DATABASE_CREATION_EXCEPTION = 1
-PROTOCOLS_DATABASE_CREATION_EXCEPTION = 2
+FATAL_ERROR = 1
+ERROR = 2
+WARNING = 3
 
 
-def check_folder_integrity():
-    """ Check is the main directory exist.
+class IntegrityCheck:
+    """ This class is responsible of check the LabNote file system integrity """
+    def __init__(self):
+        self.notebook_list = []
 
-    If it does not create the main directory and the required databases.
-    If it exist continue checking it's integrity.
+        # Start logging to a string
+        self.log_stream = StringIO()
+        format = '%(asctime)s :: %(levelname)s :: %(message)s'
+        logging.basicConfig(stream=self.log_stream, level=logging.INFO, format=format)
 
-    :returns: Exception with it's exception code[int, Exception]
-        -1 - No exception
-        0 - Main directory creation exception
-        1 - Main database creation exception
-        2 - Protocols database creation exception
-    """
-    # Check if the main directory exists
-    if not os.path.isdir(directory.DEFAULT_MAIN_DIRECTORY_PATH):
-        # Create the main directory
-        create_default_main_directory_exception = directory.create_default_main_directory()
-        if create_default_main_directory_exception:
-            return [MAIN_DIRECTORY_CREATION_EXCEPTION, create_default_main_directory_exception]
+    def full_check(self):
+        """ Check the full file system integrity
+
+        :return: If an error occured [str, int] that contains the log and error level
+        """
+        # If an error occured send it to caller
+        if not self._check_main_directory():
+            return [self.log_stream.getvalue(), FATAL_ERROR]
+        return []
+
+    @staticmethod
+    def _check_main_directory():
+        """ Check the main directory integrity """
+
+        logging.info('Checking main directory integrity...')
+
+        # Check if the main directory exists
+        if not os.path.isdir(directory.DEFAULT_MAIN_DIRECTORY_PATH):
+            logging.info('The main directory does not exist...')
+
+            try:
+                logging.info('Creating the main directory...')
+                fsentry.create_main_directory()
+            except OSError as exception:
+                logging.fatal('An error occured while creating the main directory...')
+                logging.exception(exception)
+                try:
+                    logging.info('Cleaning up the main directory...')
+                    fsentry.cleanup_main_directory()
+                except OSError as exception:
+                    logging.fatal('An error occured while cleaning up the main directory')
+                    logging.exception(exception)
+                    logging.fatal('Main directory integrity :: FAILED')
+                    return False
+                logging.fatal('Main directory integrity :: FAILED')
+                return False
+            logging.info('The main directory was created...')
+        # Check if the default subdirectories and files exists
+        elif not (os.path.isdir(directory.NOTEBOOK_DIRECTORY_PATH) and
+                  os.path.isfile(database.MAIN_DATABASE_FILE_PATH) and
+                  os.path.isfile(database.PROTOCOL_DATABASE_FILE_PATH)):
+            logging.info('The main directory is corrupted...')
+
+            try:
+                fsentry.cleanup_main_directory()
+                fsentry.create_main_directory()
+            except OSError as exception:
+                logging.fatal('An error occured while recreating the main directory')
+                logging.exception(exception)
+                logging.fatal('Main directory integrity :: FAILED')
+                return False
+        # Check if it is possible to connect with the databases
         else:
-            # Create the general database
-            create_main_database_exception = database.create_main_database()
-            if create_main_database_exception:
-                return [MAIN_DATABASE_CREATION_EXCEPTION, create_main_database_exception]
+            logging.info('Checking main directory database connectivity...')
+            try:
+                sqlite3.connect(database.MAIN_DATABASE_FILE_PATH)
+                sqlite3.connect(database.PROTOCOL_DATABASE_FILE_PATH)
+            except sqlite3.Error as exception:
+                logging.fatal('An error occured while connecting with the database...')
+                logging.exception(str(exception))
+                return False
+        logging.info('Main directory integrity :: OK')
+        return True
+
+    def _check_notebook(self):
+        """ Check that all notebook in the main database exists """
+
+        # Set return value to true
+        # This variable is changed to false if any error occurs
+        ret = True
+
+        logging.info('Checking notebook integrity...')
+
+        # Check if directories for notebooks in database exists
+        logging.info('Checking if notebook in database have a directory integrity...')
+
+        db_notebook_list = database.get_notebook_list()
+        for notebook in db_notebook_list:
+            logging.info('Checking notebook {} ...'.format(notebook['name']))
+            notebook_path = os.path.join(directory.NOTEBOOK_DIRECTORY_PATH + "/{}".format(notebook['uuid']))
+            if not os.path.isdir(notebook_path):
+                logging.info('There is no directory for notebook {} ...'.format(notebook['name']))
+                try:
+                    logging.info('Deleting notebook {} database entry...'.format(notebook['name']))
+                    database.delete_notebook(notebook['uuid'])
+                except sqlite3.Error:
+                    logging.warning('An error occured while deleting the notebook {} in the database...'
+                                    .format(notebook['name']))
+                    logging.exception(sqlite3.Error)
+                    ret = False
             else:
-                create_protocol_db_exception = database.create_protocol_db()
-                if create_protocol_db_exception:
-                    return [PROTOCOLS_DATABASE_CREATION_EXCEPTION, create_protocol_db_exception]
-            return [NO_EXCEPTION, "No exception"]
-    else:
-        return [NO_EXCEPTION, "No exception"]
+                self.notebook_list.append(notebook['uuid'])
+
+        # Check if directories have an existing notebook in the database
+        logging.info('Checking if notebook directory have a database field...')
+
+        dir_notebook_list = os.listdir(directory.NOTEBOOK_DIRECTORY_PATH)
+
+        for notebook in dir_notebook_list:
+            logging.info('Checking notebook directory {} ...'.format(notebook))
+
+            if not notebook in self.notebook_list:
+                logging.info('There is no database entry for notebook {} ...'.format(notebook))
+                try:
+                    logging.info('Deleting directory for notebook {} ...'.format(notebook))
+                    directory.delete_nb_directory(notebook)
+                except OSError as exception:
+                    logging.warning('An error occured while deleting the directory for notebook {}...'
+                                    .format(notebook))
+                    logging.exception(sqlite3.Error)
+                    ret = False
+
+        if ret:
+            logging.info('Notebook filesystem integrity :: OK')
+            return True
+        else:
+            logging.warning('Notebook filesystem integrity :: FAILED')
+            return False
