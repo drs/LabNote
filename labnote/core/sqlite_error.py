f87f3d7581e26b489e28aab05f88b6509a296231
Samuel Drouin <samuel.drouin@sdr-soft.com>
2018-07-01 15:15:35 -0400

    Added foreign key error

--- labnote/core/sqlite_error.py
+++ labnote/core/sqlite_error.py
@@ -6,6 +6,8 @@ This module is required because sqlite3 does not return the error number and onl
 # Python import
 import sqlite3
 
+FOREIGN_KEY_STR = "FOREIGN KEY constraint failed"
+FOREIGN_KEY_CODE = 787
 NOT_NULL_STR = "NOT NULL constraint failed"
 NOT_NULL_CODE = 1299
 PRIMARY_STR = "PRIMARY KEY constraint failed"
@@ -21,7 +23,9 @@ def sqlite_err_handler(err_string):
     :type err_string: str
     :return: The error number associated with the string
     """
-    if NOT_NULL_STR in err_string:
+    if FOREIGN_KEY_STR in err_string:
+        return FOREIGN_KEY_CODE
+    elif NOT_NULL_STR in err_string:
         return NOT_NULL_CODE
     elif PRIMARY_STR in err_string:
         return PRIMARY_CODE
