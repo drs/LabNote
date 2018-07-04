50bae4aad91e2a60fedbb67dbbacd41b2b606d02
Samuel Drouin <samuel.drouin@sdr-soft.com>
2018-07-04 13:42:53 -0400

    Moved TextEdit to widget module

--- LabNote/main.py
+++ LabNote/main.py
@@ -6,7 +6,7 @@ import sys
 # PyQt import
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QFont
 
 # Project import
 from labnote.interface.main_window import MainWindow
