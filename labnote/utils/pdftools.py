ffff72d6b210665156302b0263ac2117ddec0531
Samuel Drouin <samuel.drouin@sdr-soft.com>
2018-07-19 00:53:55 -0400

    Created a decent print module

--- labnote/utils/pdftools.py
+++ labnote/utils/pdftools.py
@@ -2,9 +2,8 @@
 from datetime import datetime
 
 # PyQt import
-from PyQt5.QtGui import QTextDocument, QTextCursor, QTextTableFormat, QTextBlockFormat, QTextCharFormat, QFont, QBrush
+from PyQt5.QtGui import QTextDocument, QTextCursor, QTextTableFormat, QTextLength, QTextCharFormat, QBrush
 from PyQt5.QtCore import Qt
-from PyQt5.QtPrintSupport import QPrinter
 
 
 def prepare_html_pdf(title, key, date, update, body):
@@ -27,26 +26,60 @@ def prepare_html_pdf(title, key, date, update, body):
     cursor = QTextCursor(document)
     cursor.setPosition(0)
 
-    cursor.insertHtml("""<center<font size="5">{}</font></center>""".format(title))
+    cursor.insertHtml("""<center><font size="3"><b>{}</b></font></center>""".format(title))
     cursor.insertHtml("<br>")
     table_format = QTextTableFormat()
     table_format.setCellSpacing(0)
+    table_format.setWidth(QTextLength(QTextLength.PercentageLength, 100))
     table_format.setBorderBrush(QBrush(Qt.SolidPattern))
+
+    table_char_fmt = QTextCharFormat()
+    table_char_fmt.setFontPointSize(10.5)
 
     table = cursor.insertTable(2, 2, table_format)
     table.mergeCells(1, 0, 1, 2)
-    cursor.insertText("Document : {}".format(key))
+    cursor.insertText("Document : {}\n".format(key), table_char_fmt)
     cursor.movePosition(cursor.NextCell)
-    cursor.insertText("Created : {}".format(date))
+    cursor.insertText("Created : {}".format(date), table_char_fmt)
     if update:
-        cursor.insertText("\nLast update : {}".format(update))
+        cursor.insertText("\nLast update : {}".format(update), table_char_fmt)
     cursor.movePosition(cursor.NextCell)
-    cursor.insertText("Author : Samuel Drouin - Lallemand Animal Nutrition")
+    cursor.insertText("Author : Samuel Drouin - Lallemand Animal Nutrition", table_char_fmt)
 
     cursor.movePosition(cursor.End)
+    html_start = cursor.position()
 
     cursor.insertHtml("<br>")
 
     cursor.insertHtml(body)
+
+    # Change the font family
+    fmt = QTextCharFormat()
+    fmt.setFontFamily('Verdana')
+
+    cursor.setPosition(0)
+    cursor.movePosition(cursor.End, QTextCursor.KeepAnchor)
+    cursor.mergeCharFormat(fmt)
+
+    # Make the font smaller everywhere and remove the anchors
+    position = html_start
+    cursor.setPosition(position)
+    while not cursor.atEnd():
+        cursor.setPosition(position)
+        cursor.movePosition(QTextCursor.NextCharacter, QTextCursor.KeepAnchor)
+
+        char_fmt = cursor.charFormat()
+        if cursor.charFormat().isAnchor():
+            char_fmt.setAnchor(False)
+            char_fmt.setFontUnderline(False)
+            char_fmt.setForeground(Qt.black)
+
+        if cursor.charFormat().fontPointSize() > 0:
+            char_fmt.setFontPointSize(cursor.charFormat().fontPointSize() - 3)
+        if cursor.charFormat().fontPointSize() == 10:
+            char_fmt.setFontPointSize(8)
+        cursor.mergeCharFormat(char_fmt)
+
+        position = position + 1
 
     return cursor.document().toHtml()
